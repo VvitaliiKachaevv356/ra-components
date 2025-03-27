9c0f83a63f99b70a2bd109e0983d2789b909ec74
Vitaly <VvitaliiKachaevv356@yandex.ru>
2025-03-27 17:22:52 +0400

    ShopItems

--- src/components/ShopItemClass/ShopItemClass.jsx
+++ src/components/ShopItemClass/ShopItemClass.jsx
@@ -1,35 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import "./shopItemClass.css";
+import "./shopItemFunc.css";
 
-class ShopItemClass extends Component {
-  render() {
-    const { brand, title, description, descriptionFull, price, currency } =
-      this.props.item;
+function ShopItemFunc({ item }) {
+  const { brand, title, description, descriptionFull, price, currency } = item;
 
-    return (
-      <div className="main-content">
-        <h2>{brand}</h2>
-        <h1>{title}</h1>
-        <h3>{description}</h3>
-        <div className="description">{descriptionFull}</div>
-        <div className="highlight-window mobile">
-          <div className="highlight-overlay"></div>
-        </div>
-        <div className="divider" />
-        <div className="purchase-info">
-          <div className="price">
-            {currency}
-            {price}
-          </div>
-          <button>Добавить в корзину</button>
-        </div>
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
       </div>
-    );
-  }
+      <div className="divider" />
+      <div className="purchase-info">
+        <div className="price">
+          {currency}
+          {price}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 }
 
-ShopItemClass.propTypes = {
+ShopItemFunc.propTypes = {
   item: PropTypes.shape({
     brand: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
@@ -40,4 +37,4 @@ ShopItemClass.propTypes = {
   }).isRequired,
 };
 
-export default ShopItemClass;
+export default ShopItemFunc;
--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,7 +1,27 @@
 import PropTypes from "prop-types"; // Добавьте этот импорт вверху файла
 
 function shopItemFunc({ item }) {
-  // ... (остальной код)
+  const { brand, title, description, descriptionFull, price, currency } = item;
+
+  return (
+    <div className="main-content">
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div className="description">{descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider" />
+      <div className="purchase-info">
+        <div className="price">
+          {currency}
+          {price}
+        </div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
 }
 
 shopItemFunc.propTypes = {
@@ -14,5 +34,3 @@ shopItemFunc.propTypes = {
     currency: PropTypes.string.isRequired,
   }).isRequired,
 };
-
-export default shopItemFunc;
